849c12a64680c7b459d2fa1eee1f90b965443528
cschang07 <manutdsfan@gmail.com>
2022-01-13 14:45:17 +0800

    finish GET /products for handlebars

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,16 +1,37 @@
 const db = require('../models')
 const Product = db.Product
-
+const pageLimit = 3;
 
 const productController = {
+  // getProducts: (req, res) => {
+  //   Product.findAndCountAll({ raw: true, nest: true, offset: PAGE_OFFSET, limit: PAGE_LIMIT }).then(products => {
+  //     return res.render('products', {
+  //       products
+  //     })
+  //   })
+  // },
   getProducts: (req, res) => {
-    Product.findAll({  raw: true, nest: true })
-      .then(products => {
-        console.log(products)
-        return res.render('products', {
-          products
-        })
-      })
+    let offset = 0
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+    Product.findAndCountAll({
+      raw: true, 
+      nest: true,
+      offset: offset,
+      limit: pageLimit
+    }).then(result => {
+      // data for pagination
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(result.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      //if page - 1 < 1, then it is 1, otherwise it's page -1
+      const prev = page - 1 < 1 ? 1 : page - 1
+      //if page + 1 > pages, then it is pages, otherwise it's page +1
+      const next = page + 1 > pages ? pages : page + 1
+
+      return res.render('products', { products: result, totalPage, prev, next})
+    })
   },
 }
 
--- routes/apis.js
+++ routes/apis.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const cartController = require('../controllers/cartController')
+const productController = require('../controllers/productController')
+
+
+
 
 module.exports = router
