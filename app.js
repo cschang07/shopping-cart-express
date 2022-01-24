9437abbf4c075f42a7d35104cb5c08e0c80549f0
cschang07 <manutdsfan@gmail.com>
2022-01-24 23:07:34 +0800

    working on cors 05

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const express = require('express')
-const cors = require('cors')
-const bcrypt = require('bcryptjs')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
 const handlebars = require('express-handlebars')
 const methodOverride = require('method-override')
-const session = require('express-session')
 const passport = require('./config/passport')
+const session = require('express-session')
 
 const PORT = process.env.PORT || 3000
 
@@ -47,13 +46,13 @@ app.use(passport.session())
 //   // Pass to next layer of middleware
 //   next();
 // });
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Credentials', true)
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-//   res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
-//   next()
-// })
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://mingmoth.github.io/shop-forum')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
+  next()
+})
 
 require('./routes')(app) // 0111
 
