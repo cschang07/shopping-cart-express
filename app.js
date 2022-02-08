688f3e48535510bce136b2cc30bd41fbd37a4c54
cschang07 <manutdsfan@gmail.com>
2022-02-08 20:30:32 +0800

    disable PORT for testing

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const passport = require('./config/passport')
 const path = require('path');
 const session = require('express-session')
 
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000
 
 const app = express()
 
