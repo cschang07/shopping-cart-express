a75e638ca53249b67c5daf64fa6acbc28b4a639c
cschang07 <manutdsfan@gmail.com>
2022-02-25 17:34:50 +0800

    fixing facebook 03

--- routes/auth.js
+++ routes/auth.js
@@ -7,9 +7,6 @@ router.get('/facebook', passport.authenticate('facebook', {
   scope: ['email', 'public_profile']
 }))
 
-router.get('/facebook/callback', passport.authenticate('facebook', {
-  successRedirect: '/',
-  failureRedirect: '/api/signin'
-}))
+router.get('/facebook/callback', passport.authenticate('facebook'))
 
 module.exports = router
