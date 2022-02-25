6953bc4f3e5082fa90e206f0fd92ccdbb1d73c27
cschang07 <manutdsfan@gmail.com>
2022-02-25 17:24:45 +0800

    fixing facebook passport strategy 02

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ router.get('/facebook', passport.authenticate('facebook', {
 }))
 
 router.get('/facebook/callback', passport.authenticate('facebook', {
-  successRedirect: '/api/',
+  successRedirect: '/',
   failureRedirect: '/api/signin'
 }))
 
